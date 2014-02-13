c9d039ed8246a675deb323da0590b3369cc99cfa
Dylan Lederle-Ensign <dylan.lederle.ensign@gmail.com>
2014-02-12 22:41:18 -0800

    Huzzah

---
+++ js/min-cif/Instantiation.js
@@ -0,0 +1,310 @@
+define(['min-cif/ToCLocution', 'min-cif/Rule', 'min-cif/LineOfDialogue'], function(ToCLocution, Rule, LineOfDialogue) {
+
+    /**
+     * Instantiations store the performance realization for a particular Effect
+     * brance when performing social game play.
+     * The Instantiation class aggregates lines of dialogs into Instantiations
+     * to be associated with Effects of SocialGames.
+     * @see CiF.LineOfDialog
+     */
+
+    var Instantiation = function() {
+        /**
+         * The vector of LineOfDialogue class that comprises the animations,
+         * dialogue, and timing of an instantiation.
+         */
+        this.lines = [];
+        /**
+         * The ID of the instantiation used to link social game effects with
+         * instantiations.
+         */
+        this.name;
+
+        /**
+         * The bank of conditional rules for use in performance
+         * realization tags -- especially the if/conditional tags.
+         */
+
+        this.toc1 = new ToCLocution();
+        this.toc1.tocID = 1;
+        this.toc2 = new ToCLocution();
+        this.toc2.tocID = 2;
+        this.toc3 = new ToCLocution();
+        this.toc3.tocID = 3;
+        this.id = -1;
+        this.conditionalRules = [];
+
+        this.realizeDialogue = function(initiator, responder, other) {
+            var newInst = new Instantiation();
+
+            //render toc strings
+            var newLod;
+            var locution;
+            var tocLoc;
+            var conditionalLocution;
+            var i;
+            var conditionalStringAdded;
+
+            this.lines.forEach(function(line) {
+                newLod = line.clone();
+
+                newLod.initiatorLine = "";
+                newLod.responderLine = "";
+                newLod.otherLine = "";
+
+                line.initiatorLocutions.forEach(function(locution) {
+                    toc1.speaker = "initiator";
+                    toc2.speaker = "initiator";
+                    toc3.speaker = "initiator";
+
+                    toc1.renderText(initiator, responder, other, line);
+                    toc2.renderText(initiator, responder, other, line);
+                    toc3.renderText(initiator, responder, other, line);
+                    if (locution.getType() == "ToCLocution") {
+                        tocLoc = locution;
+                        if (tocLoc.tocID == 1) {
+                            newLod.initiatorLine += toc1.realizedString;
+                        }
+                        else if (tocLoc.tocID == 2) {
+                            newLod.initiatorLine += toc2.realizedString;
+                        }
+                        else if (tocLoc.tocID == 3) {
+                            newLod.initiatorLine += toc3.realizedString;
+                        }
+                    }
+                    else if (locution.getType() == "MixInLocution") {
+                        initiator.isSpeakerForMixInLocution = true;
+                        newLod.initiatorLine += locution.renderText(initiator, responder, other, line);
+                        initiator.isSpeakerForMixInLocution = false;
+                    }
+                    else if (locution.getType() == "ConditionalLocuation") {
+                        conditionalLocution = locution;
+                        conditionalStringAdded = false;
+
+                        if (conditionalLocution.ifRuleID < this.conditionalRules.length) {
+                            if (this.conditionalRules[conditionalLocution.ifRuleID].evaluate(initiator, responder, other)) {
+                                newLod.initiatorLine += conditionalLocution.ifRuleString;
+                                conditionalStringAdded = true;
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            for (i = 0; i < conditionalLocution.elseIfRuleIDs.length; i++ ) {
+                                if (conditionalLocution.elseIfRuleIDs[i] > this.conditionalRules.length - 1) continue;
+                                if (this.conditionalRules[conditionalLocution.elseIfRuleIDs[i]].evaluate(initiator, responder, other)) {
+                                    newLod.initiatorLine += conditionalLocution.elseIfStrings[i];
+                                    conditionalStringAdded = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            newLod.initiatorLine += conditionalLocution.elseString;
+                        }
+
+                    }
+                    else {
+                        if (locution.getType() == "SFDBLabelLocution") {
+                            locution.speaker = "i";
+                        }
+                        //console.debug(this, "realizeDialogue(): initiator locution: " + l);
+                        newLod.initiatorLine += locution.renderText(initiator, responder, other, line);
+                    }
+                });
+                //make the responder line
+
+                line.responderLocutions.forEach(function(locution) {
+                    toc1.speaker = "responder";
+                    toc2.speaker = "responder";
+                    toc3.speaker = "responder";
+
+                    toc1.renderText(initiator, responder, other, line);
+                    toc2.renderText(initiator, responder, other, line);
+                    toc3.renderText(initiator, responder, other, line);
+                    if (locution.getType() == "ToCLocution") {
+                        tocLoc = locution;
+                        if (tocLoc.tocID == 1) {
+                            newLod.responderLine += toc1.realizedString;
+                        }
+                        else if (tocLoc.tocID == 2) {
+                            newLod.responderLine += toc2.realizedString;
+                        }
+                        else if (tocLoc.tocID == 3) {
+                            newLod.responderLine += toc3.realizedString;
+                        }
+                    }
+                    else if (locution.getType() == "MixInLocution") {
+                        responder.isSpeakerForMixInLocution = true;
+                        newLod.responderLine += locution.renderText(initiator, responder, other, line);
+                        responder.isSpeakerForMixInLocution = false;
+                    }
+                    else if (locution.getType() == "ConditionalLocuation") {
+                        conditionalLocution = locution;
+                        conditionalStringAdded = false;
+
+                        if (conditionalLocution.ifRuleID < this.conditionalRules.length) {
+                            if (this.conditionalRules[conditionalLocution.ifRuleID].evaluate(initiator, responder, other)) {
+                                newLod.responderLine += conditionalLocution.ifRuleString;
+                                conditionalStringAdded = true;
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            for (i = 0; i < conditionalLocution.elseIfRuleIDs.length; i++ ) {
+                                if (conditionalLocution.elseIfRuleIDs[i] > this.conditionalRules.length - 1) continue;
+                                if (this.conditionalRules[conditionalLocution.elseIfRuleIDs[i]].evaluate(initiator, responder, other)) {
+                                    newLod.responderLine += conditionalLocution.elseIfStrings[i];
+                                    conditionalStringAdded = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            newLod.responderLine += conditionalLocution.elseString;
+                        }
+
+                    }
+                    else {
+                        if (locution.getType() == "SFDBLabelLocution") {
+                            locution.speaker = "r";
+                        }
+
+                        //console.debug(this, "realizeDialogue(): initiator locution: " + l);
+                        newLod.responderLine += locution.renderText(initiator, responder, other,line);
+                    }
+                });
+                //make the other line
+                line.otherLocutions.forEach(function(locution) {
+                    toc1.speaker = "other";
+                    toc2.speaker = "other";
+                    toc3.speaker = "other";
+
+                    toc1.renderText(initiator, responder, other, line);
+                    toc2.renderText(initiator, responder, other, line);
+                    toc3.renderText(initiator, responder, other, line);
+                    if (locution.getType() == "ToCLocution") {
+                        tocLoc = locution;
+                        if (tocLoc.tocID == 1) {
+                            newLod.otherLine += toc1.realizedString;
+                        }
+                        else if (tocLoc.tocID == 2) {
+                            newLod.otherLine += toc2.realizedString;
+                        }
+                        else if (tocLoc.tocID == 3) {
+                            newLod.otherLine += toc3.realizedString;
+                        }
+                    }
+                    else if (locution.getType() == "MixInLocution") {
+                        other.isSpeakerForMixInLocution = true;
+                        newLod.otherLine += locution.renderText(initiator, responder, other, line);
+                        other.isSpeakerForMixInLocution = false;
+                    }
+                    else if (locution.getType() == "ConditionalLocuation") {
+                        conditionalLocution = locution;
+                        conditionalStringAdded = false;
+
+                        if (conditionalLocution.ifRuleID < this.conditionalRules.length) {
+                            if (this.conditionalRules[conditionalLocution.ifRuleID].evaluate(initiator, responder, other)) {
+                                newLod.otherLine += conditionalLocution.ifRuleString;
+                                conditionalStringAdded = true;
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            for (i = 0; i < conditionalLocution.elseIfRuleIDs.length; i++ ) {
+                                if (conditionalLocution.elseIfRuleIDs[i] > this.conditionalRules.length - 1) continue;
+                                if (this.conditionalRules[conditionalLocution.elseIfRuleIDs[i]].evaluate(initiator, responder, other)) {
+                                    newLod.otherLine += conditionalLocution.elseIfStrings[i];
+                                    conditionalStringAdded = true;
+                                    break;
+                                }
+                            }
+                        }
+
+                        if (!conditionalStringAdded) {
+                            newLod.otherLine += conditionalLocution.elseString;
+                        }
+
+                    }
+                    else {
+                        if (locution.getType() == "SFDBLabelLocution") {
+                            locution.speaker = "o";
+                        }
+
+                        //console.debug(this, "realizeDialogue(): initiator locution: " + l);
+                        newLod.otherLine += locution.renderText(initiator, responder, other, line);
+                    }
+                });
+                newInst.lines.push(newLod);
+            });
+
+            return newInst;
+        }
+
+
+        this.requiresOtherToPerform = function() {
+            this.lines.forEach(function(lod) {
+                if (lod.otherApproach || lod.otherLine != "") {
+                    return true;
+                }
+            });
+            return false;
+        }
+
+        /**********************************************************************
+         * Utility Functions
+         *********************************************************************/
+
+        this.toString = function() {
+            var returnstr = "";
+            for (var i = 0; i < this.lines.length; ++i) {
+                returnstr += this.lines[i];
+                if (i < this.lines.length - 1) {
+                    //returnstr += "^";
+                }
+                returnstr += "\n";
+            }
+            return returnstr;
+        }
+
+        this.clone = function() {
+            var i = new Instantiation();
+            i.id = this.id;
+            i.name = this.name;
+            i.lines = this.lines.slice(0);
+
+            this.conditionRules.forEach(function(r) {
+                i.conditionalRules.push(r.clone());
+            });
+
+
+            i.toc1.rawString = this.toc1.rawString;
+            i.toc2.rawString = this.toc2.rawString;
+            i.toc3.rawString = this.toc3.rawString;
+
+            return i;
+        }
+    } //End of Instantiation
+
+    Instantiation.equals = function(x, y) {
+        if (x.id != y.id) return false;
+        if (x.name != y.name) return false;
+        if (x.lines.length != y.lines.length) return false;
+        if (x.toc1.rawString != y.toc1.rawString) return false;
+        if (x.toc2.rawString != y.toc2.rawString) return false;
+        if (x.toc3.rawString != y.toc3.rawString) return false;
+
+        for (var i = 0; i < x.conditionalRules.length; ++i) {
+            if (!Rule.equals(x.conditionalRules[i], y.conditionalRules[i])) return false;
+        }
+
+        for (i = 0; i < x.lines.length; ++i) {
+            if (!LineOfDialogue.equals(x.lines[i], y.lines[i])) return false;
+        }
+        return true;
+    }
+
+    return Instantiation;
+});
---
+++ js/min-cif/InstrumentedInfluenceRule.js
@@ -0,0 +1,229 @@
+define(['min-cif/InfluenceRule', 'min-cif/Predicate', 'min-cif/CiFSingleton', 'min-cif/SocialFactsDB'], function(InfluenceRule, Predicate, CiFSingleton, SocialFactsDB) {
+    var InstrumentedInfluenceRule = function() {
+        InstrumentedInfluenceRule.counter++;
+
+        this.uniqueID = counter;
+        this.history =  {};
+
+        if (!InstrumentedInfluenceRule.rulesByID) {
+            InstrumentedInfluenceRule.rulesByID = {};
+        }
+        InstrumentedInfluenceRule.rulesByID[this.uniqueID] = this;
+
+        this.evaluate = function(initiator, responder, other, sg) {
+            this.lastTrueCount = 0;
+            var p;
+            var p1;
+            var i = 0;
+            var truthState = {};
+
+            var isTrue = false;
+
+            if (0 < this.highestSFDBOrder()) {
+                //console.debug(this, "evaluate() this.highestSFDBOrder of the rule: " + this.highestSFDBOrder());
+                return this.evaluateTimeOrdedRule(initiator, responder, other);
+            }
+
+            this.predicates.forEach(function(p) {
+
+                //console.debug(this, "evaluating predicate of type: " + Predicate.getNameByType(p.type), 0);
+                var startTime = getTimer();
+                if (!p.evaluate(initiator, responder, other, sg)) {
+                    Predicate.evalutionComputationTime += getTimer() - startTime;
+                    //console.debug(this, "predicate is false - " + p.toString());
+                    truthState[i] = false;
+                } else {
+                    truthState[i] = true;
+                    ++this.lastTrueCount;
+                }
+                Predicate.evalutionComputationTime += getTimer() - startTime;
+
+                ++i;
+            });
+
+            if(this.lastTrueCount == this.predicates.length)
+                isTrue = true
+            else
+                isTrue = false;
+
+            truthState["numTrue"] = this.lastTrueCount;
+            truthState["result"] = isTrue;
+            this.addTruthStateToHistory(initiator, responder, other, truthState);
+
+            return isTrue;
+        }
+
+        this.evaluateTimeOrdedRule = function(x, y, z) {
+            var cif = CiFSingleton.getInstance();
+            //The max order value of the rule.
+            var maxOrderInRule = this.highestSFDBOrder();
+            //when evaluating an order, this is value is updated with the highest truth time for the order.
+            //when evaluating an order, this is value is updated with the highest truth time for the order.
+            var curOrderTruthTime = cif.sfdb.getLowestContextTime();
+            //the highest truth time of all the predicates in the previous order.
+            var lastOrderTruthTime = curOrderTruthTime;
+            //the truth time of the current predicate.
+            var time;
+            //the current order being evaluated
+            var order;
+            var pred; //predicate iterator
+            var startTime;
+
+            var truthState = {};
+            var i = 0;
+            var numTrue = 0;
+
+            var isTrue = false;
+
+            for (order = 1; order <= maxOrderInRule; ++order) {
+                this.predicates.forEach(function(pred) {
+                    if (pred.sfdbOrder == order) {
+                        startTime = getTimer();
+                        //the predicate is of the order we are currently concerned with
+                        time = cif.sfdb.timeOfPredicateInHistory(pred, x, y, z);
+                        Predicate.evalutionComputationTime += getTimer() - startTime;
+                        //was the predicate true at all in history? If not, return false.
+                        if ((SocialFactsDB.PREDICATE_NOT_FOUND == time) || (time < lastOrderTruthTime)) {
+                            truthState[i] = false;
+                        }else {
+                            truthState[i] = true;
+                            numTrue++
+                        }
+                        ++i;
+
+                        //update curOrderTruthTime to highest value for this order
+                        if (time > curOrderTruthTime) curOrderTruthTime = time;
+                    }
+                });
+                lastOrderTruthTime = curOrderTruthTime;
+            }
+
+            //evaluate the predicates in the rule that are not time sensitive (i.e. their order is less than 1).
+            this.predicates.forEach(function(pred) {
+                if (pred.isSFDB < 1) {
+                    startTime = getTimer();
+                    if (!pred.evaluate(x, y, z)) {
+                        Predicate.evalutionComputationTime += getTimer() - startTime;
+                        truthState[i] = false;
+                    }else {
+                        numTrue++
+                truthState[i] = true;
+                    }
+                    ++i;
+                    Predicate.evalutionComputationTime += getTimer() - startTime;
+                }
+            });
+
+            if(numTrue == this.predicates.length)
+                isTrue = true
+            else
+                isTrue = false;
+
+            truthState["numTrue"] = numTrue;
+            truthState["result"] = isTrue;
+            this.addTruthStateToHistory(x, y, z, truthState);
+
+            return isTrue;
+        }
+
+        /**
+         * Prepares the history dictionaries to accept this rule. It properly links the dictionary entries including
+         * the truth state.
+         * @param	x	First character variable.
+         * @param	y	Second character variable.
+         * @param	z	Third character variable.
+         * @param	truthState	The state of evaluation of the rules of one evaluation.
+         */
+        var addTruthStateToHistory = function(x, y, z, truthState) {
+            var time = CiFSingleton.getInstance().time;
+            //is there a dictionary for this time?\
+
+            var zName = (z)?z.characterName:"none";
+            if (!this.history[time]) {
+                this.history[time] = {};
+            }
+
+            //is there a dictionary for this x?
+            if (!this.history[time][x.characterName]) {
+                this.history[time][x.characterName] = {};
+            }
+
+            //is there a dictionary for this y?
+            if (!this.history[time][x.characterName][y.characterName]) {
+                this.history[time][x.characterName][y.characterName] = {};
+            }
+
+            //is there a dictionary for this z?
+            if (!this.history[time][x.characterName][y.characterName][zName]) {
+                this.history[time][x.characterName][y.characterName][zName] = {};
+            }
+
+            //Add the truthState.
+            this.history[time][x.characterName][y.characterName][zName] = truthState;
+        }
+
+        var singleRuleHistoryToString = function(id) {
+            //the truth state
+            var t;
+            var output = "";
+            var curHistory = InstrumentedInfluenceRule.rulesByID[id].history;
+            var rulePredicateCount = InstrumentedInfluenceRule.rulesByID[id].predicates.length;
+            curHistory.forEach(function(time) {
+                curHistory[time].forEach(function(xName) {
+                    curHistory[time][xName].forEach(function(yName) {
+                        curHistory[time][xName][yName].forEach(function(zName) {
+                            //for (var ts:* in curHistory[time][xName][yName][zName]) {
+                            //t = ts as Dictionary;
+                            t = curHistory[time][xName][yName][zName];
+                            //at the truth state level here
+                            output += "\t" + time + ", " + xName + ", " + yName + ", " + zName + ", " + t["result"] + ", " + t["numTrue"];
+                            for (var i = 0; i < rulePredicateCount; ++i ) {
+                                output += ", " + t[i];
+                            }
+                            output += "\n";
+                            //}
+                        });
+                    });
+                });
+            });
+            if ("" == output) {
+                output = id + " was never evaluated. " + InstrumentedInfluenceRule.rulesByID[id] + "\n";
+            } else {
+                output = id + ": " + InstrumentedInfluenceRule.rulesByID[id] + "\n" + output;
+            }
+            return output;
+
+        }
+    } //End of InstrumentedInfluenceRule
+    InstrumentedInfluenceRule.prototype = new InfluenceRule();
+    InstrumentedInfluenceRule.counter = 0;
+    /**
+     * Holds reference to all InstrumentedInfluenceRules by keys of their unique IDs.
+     */
+    InstrumentedInfluenceRule.rulesByID = {};
+    /**
+     * Returns a string containing either one or all of the rules and their histories.
+     * @param	id	The unique identified of a rule. The default value is -1 and it
+     * tells the function to print the history of all rules.
+     * @return	A string containing the history of one or more rules.
+     */
+    InstrumentedInfluenceRule.ruleHistoryToString = function(id) {
+        var output = "";
+        var iir;
+        var i = 0;
+        var truthState;
+        RET
+
+            if (id >= 0) {
+                return singleRuleHistoryToString(id);
+            }
+        InstrumentedInfluenceRule.rulesByID.forEach(function(IDKey) {
+            output += singleRuleHistoryToString(IDkey) + "\n";
+        });
+
+        return output;
+    }
+
+
+    return InstrumentedInfluenceRule;
+});
